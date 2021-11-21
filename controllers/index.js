c224e8cd7008e4e85e4654b0f7b1db95182ac3d3
azuryte5 <85147307+azuryte5@users.noreply.github.com>
2021-11-21 08:30:52 -0500

    simple handlebars added

--- controllers/index.js
+++ controllers/index.js
@@ -2,10 +2,10 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 
-// when homepage routes need to be made
-// const homeRoutes = require('./home-routes.js');
-// router.use('/', homeRoutes);
+// when dashboard add those later
+const homeRoutes = require('./home-routes.js');
 
+router.use('/', homeRoutes);
 router.use('/api', apiRoutes);
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,11 +1,14 @@
 const express = require('express');
 //exphbs is like a build a bear templating engine
 const exphbs = require('express-handlebars');
+const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sequelize = require("./config/connection");
+// sessions coming soon
+
 
 const hbs = exphbs.create({});
 
@@ -13,12 +16,14 @@ app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(require('./controllers/'));
 
-app.get('/', (req, res) => {
-    res.render('layouts/main');
-  });
+// app.get('/', (req, res) => {
+//     res.render('layouts/main');
+//   });
 // force true every time server restarts it restarts the cookies
 // turn it false when program is ready.
 sequelize.sync({ force: false }).then(() =>{

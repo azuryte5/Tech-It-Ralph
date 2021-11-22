d9eb3404c9a291509e89cdfe2933dbd021a3ebe2
azuryte5 <85147307+azuryte5@users.noreply.github.com>
2021-11-22 14:39:27 -0500

    routes updated and test heroku

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
-const withAuth = require('../../utils/auth');
 const { User, Post, Comment } = require('../../models');
 
 // GET all users for Insomnia 
